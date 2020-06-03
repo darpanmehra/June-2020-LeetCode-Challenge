a1c4ebc7193632cb69bca58536b3d742cea3a47b
Darpan Mehra <darpan.mehra10@gmail.com>
2020-06-03 19:13:04 +0530

    June 03, 2020 Problem - Two City Scheduling

--- June 02, 2020.py
+++ June 02, 2020.py
@@ -28,5 +28,5 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        node.val = node.next.val
+        node.val = node.next
         node.next = node.next.next
