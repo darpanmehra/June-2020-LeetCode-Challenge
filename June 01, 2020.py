532eed8c2e11a0ccbb43770d0ddbdbfe433af2d3
Darpan Mehra <darpan.mehra10@gmail.com>
2020-06-03 17:39:37 +0530

    June 01, 2020 - Invert Binary Tree

---
+++ June 01, 2020.py
@@ -0,0 +1,31 @@
+"""
+Invert Binary Tree
+
+Invert a binary tree.
+Example:
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if root:
+            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+            return root
+        return None
