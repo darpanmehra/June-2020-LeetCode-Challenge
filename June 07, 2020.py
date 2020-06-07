193fd26de5b885d9b3052b801b6718980b950740
Darpan Mehra <darpan.mehra10@gmail.com>
2020-06-07 20:29:29 +0530

    June 07, 2020 Problem - Coin Change 2

---
+++ June 07, 2020.py
@@ -0,0 +1,34 @@
+"""
+Coin Change 2
+
+You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
+
+Example 1:
+Input: amount = 5, coins = [1, 2, 5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+Example 2:
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+Example 3:
+Input: amount = 10, coins = [10]
+Output: 1
+"""
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        combinations = [0] * (amount+1)
+        combinations[0] = 1
+        for coin in coins:
+            for combi in range(coin, amount+1):
+                if combi >= coin:
+                    combinations[combi] += combinations[combi - coin]
+        return combinations[amount]
